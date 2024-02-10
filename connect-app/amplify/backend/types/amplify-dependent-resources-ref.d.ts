63834657cf947bf8bb841efbaff9dfdf95ee39bf
rachel-reese <98109102+rachel-reese@users.noreply.github.com>
2024-02-10 10:45:40 -0500

    Delete connect-app directory

--- connect-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "connectapp": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
