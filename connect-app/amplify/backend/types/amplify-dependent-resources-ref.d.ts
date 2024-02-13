b1e6545cc2154aed7aa55b03e93fcb2144a0cd74
Rachel Reese <rachel.reese@bison.howard.edu>
2024-02-13 11:19:00 -0500

    added auth

---
+++ connect-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "group13awsconnect91f8d8a4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
