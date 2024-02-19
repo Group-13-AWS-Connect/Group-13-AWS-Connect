fa47cb227218030e8133be14844111807978a946
davidobi2911 <davidobinna2911@gmail.com>
2024-02-19 13:29:33 -0500

    Setting up the Nav Bar

--- sp-aws-connect/src/index.js
+++ sp-aws-connect/src/index.js
@@ -5,27 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@aws-amplify/ui-react/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< Updated upstream
-=======
 import Navigation from './Navigation';
 import { BrowserRouter } from 'react-router-dom';
->>>>>>> Stashed changes
+
 // import '@aws-amplify/ui-react/styles.layer.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< Updated upstream
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
+
   <BrowserRouter> 
     <React.StrictMode>
       <Navigation />
     </React.StrictMode>
   </BrowserRouter>
->>>>>>> Stashed changes
+
 );
 
 // If you want to start measuring performance in your app, pass a function
