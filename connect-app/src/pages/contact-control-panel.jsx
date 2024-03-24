4d692b84faa86197cd2bbc647f56ebccc4bf76ca
Rachel Reese <rachel.reese@bison.howard.edu>
2024-03-24 19:11:42 -0400

    CCP test

--- connect-app/src/pages/contact-control-panel.jsx
+++ connect-app/src/pages/contact-control-panel.jsx
@@ -1,5 +1,44 @@
 export function Contact_Control_Panel() {
     return (
-        <h1> Contact control panel </h1>
+        <body onload="init()">
+            <div id="container-div" style="width: 400px; height: 800px;"></div>
+            <script type="text/javascript">
+            var containerDiv = document.getElementById("container-div");
+            var instanceURL = "https://my-instance-domain.my.connect.aws/ccp-v2/";
+            // initialize the streams api
+            function init() {
+                // initialize the ccp
+                connect.core.initCCP(containerDiv, {
+                ccpUrl: instanceURL,            // REQUIRED
+                loginPopup: true,               // optional, defaults to `true`
+                loginPopupAutoClose: true,      // optional, defaults to `false`
+                loginOptions: {                 // optional, if provided opens login in new window
+                    autoClose: true,              // optional, defaults to `false`
+                    height: 600,                  // optional, defaults to 578
+                    width: 400,                   // optional, defaults to 433
+                    top: 0,                       // optional, defaults to 0
+                    left: 0                       // optional, defaults to 0
+                },
+                region: "eu-central-1",         // REQUIRED for `CHAT`, optional otherwise
+                softphone: {                    // optional, defaults below apply if not provided
+                    allowFramedSoftphone: true,   // optional, defaults to false
+                    disableRingtone: false,       // optional, defaults to false
+                    ringtoneUrl: "[your-ringtone-filepath].mp3", // optional, defaults to CCP’s default ringtone if a falsy value is set
+                    allowFramedVideoCall: true,    // optional, default to false
+                    allowEarlyGum: true    //optional, default to true
+                },
+                pageOptions: { //optional
+                    enableAudioDeviceSettings: false, //optional, defaults to 'false'
+                    enableVideoDeviceSettings: false, //optional, defaults to 'false'
+                    enablePhoneTypeSettings: true //optional, defaults to 'true' 
+                },
+                shouldAddNamespaceToLogs: false, //optional, defaults to 'false'
+                ccpAckTimeout: 5000, //optional, defaults to 3000 (ms)
+                ccpSynTimeout: 3000, //optional, defaults to 1000 (ms)
+                ccpLoadTimeout: 10000 //optional, defaults to 5000 (ms)
+                })
+            }
+            </script>
+        </body>
     )
 }
