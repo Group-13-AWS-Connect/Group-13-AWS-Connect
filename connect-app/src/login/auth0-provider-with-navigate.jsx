6197437699638c71bd77fa84c6d9d8fc753a2c97
Rachel Reese <rachel.reese@bison.howard.edu>
2024-04-10 21:30:56 -0400

    trying to fix callback and react-router-dom still

--- connect-app/src/login/auth0-provider-with-navigate.jsx
+++ connect-app/src/login/auth0-provider-with-navigate.jsx
@@ -1,34 +1,34 @@
-import { Auth0Provider } from "@auth0/auth0-react";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { Auth0Provider } from "@auth0/auth0-react";
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
 
-export const Auth0ProviderWithNavigate = ({ children }) => {
-  const navigate = useNavigate();
+// export const Auth0ProviderWithNavigate = ({ children }) => {
+//   const navigate = useNavigate();
 
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+//   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+//   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+//   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
 
-  const onRedirectCallback = (appState) => {
-    navigate(appState?.returnTo || window.location.pathname);
-  };
+//   const onRedirectCallback = (appState) => {
+//     navigate(appState?.returnTo || window.location.pathname);
+//   };
 
-  if (!(domain && clientId && redirectUri)) {
-    console.log(import.meta.env.VITE_AUTH0_DOMAIN, import.meta.env.VITE_AUTH0_CLIENT_ID, import.meta.env.VITE_AUTH0_CALLBACK_URL);
+//   if (!(domain && clientId && redirectUri)) {
+//     console.log(import.meta.env.VITE_AUTH0_DOMAIN, import.meta.env.VITE_AUTH0_CLIENT_ID, import.meta.env.VITE_AUTH0_CALLBACK_URL);
 
-    return null;
-  }
+//     return null;
+//   }
 
-  return (
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{
-        redirect_uri: redirectUri,
-      }}
-      onRedirectCallback={onRedirectCallback}
-    >
-      {children}
-    </Auth0Provider>
-  );
-};
+//   return (
+//     <Auth0Provider
+//       domain={domain}
+//       clientId={clientId}
+//       authorizationParams={{
+//         redirect_uri: redirectUri,
+//       }}
+//       onRedirectCallback={onRedirectCallback}
+//     >
+//       {children}
+//     </Auth0Provider>
+//   );
+// };
