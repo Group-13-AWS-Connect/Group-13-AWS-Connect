a94990868b95212becb5deb0bbe6dda70004c1af
Rachel Reese <rachel.reese@bison.howard.edu>
2024-04-12 04:12:05 -0400

    fixing more

--- connect-app/src/recordings/recordingsData.js
+++ connect-app/src/recordings/recordingsData.js
@@ -19,23 +19,95 @@ const input = { // ListObjectsRequest
 const command = new ListObjectsCommand(input);
 const response = await client.send(command);
 
+// const input_other_bucket = { // ListObjectsRequest
+//   Bucket: "bison-wireless-storage",
+//   Delimiter: "/",
+//   Prefix: "ContactRecords2024/04/05/12/",
+// };
+
+// const command_other_bucket = new ListObjectsCommand(input_other_bucket);
+// const response_other_bucket = await client.send(command_other_bucket);
+
+// var response_new = {...response.Contents,...response_other_bucket.Contents};
+// console.log(response_new)
+
+var recordings_list = [];
+var call_review_json = {};
+
+for (let i = 0; i < response.Contents.length; i++){
+  var file_location = 'https://bison-wireless-storage.s3.amazonaws.com/' + response.Contents[i].Key;
+  var contact_record = await fetch(file_location)
+  .then ((response) => response.json())
+  .then (data => {
+    return data;
+  });
+
+  var conversation_duration = await fetch('https://bison-wireless-storage.s3.amazonaws.com/Analysis/Voice/2024/04/05/' + contact_record.ContactId + "_analysis_" + contact_record.Queue.DequeueTimestamp + ".json")
+  .then ((response) => response.json())
+  .then (data => {
+    return data.ConversationCharacteristics.TotalConversationDurationMillis;
+  });
+
+  let conversation_mins = conversation_duration/1000/60;
+  conversation_mins = Math.floor(conversation_mins);
+  let conversation_secs = (conversation_duration/1000)%60;
+  conversation_secs = Math.floor(conversation_secs);
+  var convo_length = `${conversation_mins}:${conversation_secs}`;
+
+  var user_id = contact_record.Agent.ARN;
+  user_id = user_id.split("/");
+  user_id = user_id[(user_id.length - 1)];
+
+  const connect_client = new ConnectClient({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: `${import.meta.env.VITE_AWS_ACCESS_KEY_ID}N`,
+      secretAccessKey: `${import.meta.env.VITE_AWS_SECRET_ACCESS_KEY}x`
+    }
+  });
+  const connect_input = { 
+    InstanceId: "4d9f7918-6f2c-45a1-a403-27493400fc91",
+    UserId: user_id
+  };
+  
+  
+  const connect_command = new DescribeUserCommand(connect_input);
+  const connect_response = await connect_client.send(connect_command);
+  
+  // console.log(connect_response);
+  var date = Date.parse(contact_record.InitiationTimestamp)
+  var date_fixed = new Date(date)
+
+  var curr_recording = {
+    name: `${connect_response.User.IdentityInfo.FirstName} ${connect_response.User.IdentityInfo.LastName}`,
+    id: contact_record.Agent.Username,
+    date: String(date_fixed).slice(0, 15),
+    full_date: date_fixed.toString(),
+    time: convo_length,
+    screen_recording: 'https://bison-wireless-storage.s3.amazonaws.com' + contact_record.Recordings[1].Location.slice(22),
+    contact_id: contact_record.ContactId,
+    audio_file: 'https://bison-wireless-storage.s3.amazonaws.com/Analysis/Voice/Redacted' + contact_record.Recordings[0].Location.slice(60,72) + contact_record.ContactId + "_call_recording_redacted_" + contact_record.Queue.DequeueTimestamp + ".wav",
+    transcript: 'https://bison-wireless-storage.s3.amazonaws.com/Analysis/Voice/Redacted' + contact_record.Recordings[0].Location.slice(60,72) + contact_record.ContactId + "_analysis_redacted_" + contact_record.Queue.DequeueTimestamp + ".json",
+  }
+
+  // console.log(contact_record.Recordings[0].Location.slice(60,72))
+  recordings_list.push(curr_recording);
+  call_review_json[contact_record.ContactId] = curr_recording;
+
+
+}
+
 const input_other_bucket = { // ListObjectsRequest
   Bucket: "bison-wireless-storage",
   Delimiter: "/",
-  Prefix: "ContactRecords2024/04/05/10/",
+  Prefix: "ContactRecords2024/04/05/12/",
 };
 
 const command_other_bucket = new ListObjectsCommand(input_other_bucket);
 const response_other_bucket = await client.send(command_other_bucket);
 
-var response_new = {...response.Contents,...response_other_bucket.Contents};
-console.log(response_new)
-
-var recordings_list = [];
-var call_review_json = {};
-
-for (let i = 0; i < response_new.length; i++){
-  var file_location = 'https://bison-wireless-storage.s3.amazonaws.com/' + response_new[i].Key;
+for (let i = 0; i < response_other_bucket.Contents.length; i++){
+  var file_location = 'https://bison-wireless-storage.s3.amazonaws.com/' + response_other_bucket.Contents[i].Key;
   var contact_record = await fetch(file_location)
   .then ((response) => response.json())
   .then (data => {
